fab5714b0475c8e90c68a7ab4d037466ce1a692b
ERAS <arannierymariano@gmail.com>
2024-05-29 10:22:03 -0300

    home

--- Home.php
+++ Home.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <me,ta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/home.css">
     <title>HOME</title>
 </head>
 <body>
     
-  
+  <h1>Sejam bem vindos a plataforma ERAS</h1>
            
     
 </body>
